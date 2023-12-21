b3eeb99aa23722e8c166d115690dc8863796f4e0
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-20 23:09:34 -0400

    task9. added strict_slashes to routes and make response

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Amenities API endpoints"""
-from flask import jsonify, abort, request
+from flask import Flask, jsonify, abort, request, make_response
 from models import storage
 from models.amenity import Amenity
 from api.v1.views import app_views
 
 
-@app_views.route('/amenities', methods=['GET'])
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Retrieve all Amenity objects"""
     amenities = [amenity.to_dict() for amenity
@@ -14,7 +14,7 @@ def get_amenities():
     return jsonify(amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieve a specific Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -23,18 +23,18 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """Delete a specific Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    storage.delete(amenity)
+    amenity.delete()
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route('/amenities', methods=['POST'])
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
     """Create a new Amenity object"""
     if not request.get_json():
@@ -43,10 +43,10 @@ def create_amenity():
         abort(400, description="Missing name")
     amenity = Amenity(**request.get_json())
     amenity.save()
-    return jsonify(amenity.to_dict()), 201
+    return make_response(jsonify(amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """Update a specific Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
