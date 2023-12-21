499b62e408b06fa730904cd3b7ada05f6518f395
nadeshda18 <nadeshda02@hotmail.com>
2023-12-20 23:12:32 -0400

    pycodestyle

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -14,7 +14,8 @@ def get_amenities():
     return jsonify(amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieve a specific Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -23,7 +24,8 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_amenity(amenity_id):
     """Delete a specific Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -46,7 +48,8 @@ def create_amenity():
     return make_response(jsonify(amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """Update a specific Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
